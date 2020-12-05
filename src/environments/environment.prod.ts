52887b16340461e52505396ba67a00487b79972d
Yusuke Gunji <inochideutae@gmail.com>
2020-12-05 12:27:59 +0900

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDszMDyEGhEXTCCPH11tCL9eL5U3lBIXsg',
+    authDomain: 'poo-buster.firebaseapp.com',
+    projectId: 'poo-buster',
+    storageBucket: 'poo-buster.appspot.com',
+    messagingSenderId: '715285656316',
+    appId: '1:715285656316:web:9796e4f1ac229c2b175d79',
+    measurementId: 'G-8VTMBMDM7M',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDszMDyEGhEXTCCPH11tCL9eL5U3lBIXsg',
+    authDomain: 'poo-buster.firebaseapp.com',
+    projectId: 'poo-buster',
+    storageBucket: 'poo-buster.appspot.com',
+    messagingSenderId: '715285656316',
+    appId: '1:715285656316:web:9796e4f1ac229c2b175d79',
+    measurementId: 'G-8VTMBMDM7M',
+  },
 };
 
 /*
