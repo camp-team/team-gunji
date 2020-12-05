1591f18f4e3844f0ca1a059394945518515a3a6c
chamaboy <hiroponpoko07@gmail.com>
2020-12-05 18:15:34 +0900

    機能：ユーザー作成 Fixes #10

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserData } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  user$: Observable<UserData> = this.authService.user$;
+  uid: string | any;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 }
