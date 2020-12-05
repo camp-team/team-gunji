71ae0c69a4737d07e1e5d08cc2a56e7cd19e733d
Yusuke Gunji <inochideutae@gmail.com>
2020-12-05 21:43:51 +0900

    feat(welcome): ルーティング追加

--- src/app/welcome/welcome-routing.module.ts
+++ src/app/welcome/welcome-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateHomeComponent } from './create-home/create-home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {
     path: '',
     component: WelcomeComponent,
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
+  {
+    path: 'create-home',
+    component: CreateHomeComponent,
   },
 ];
 
