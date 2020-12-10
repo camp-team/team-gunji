18ddd12f7933b18f5d367b1b5d9fc023ea9e151d
Yusuke Gunji <inochideutae@gmail.com>
2020-12-10 10:07:00 +0900

    feat(room): poo表示スタイル追加とrouting.param追加

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RoomRoutingModule } from './room-routing.module';
 import { RoomComponent } from './room/room.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [RoomComponent],
-  imports: [CommonModule, RoomRoutingModule, MatButtonModule],
+  imports: [CommonModule, RoomRoutingModule, MatButtonModule, MatIconModule],
 })
 export class RoomModule {}
--- src/app/room/room/room.component.ts
+++ src/app/room/room/room.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
+import { bounce, fade, float } from 'src/app/animations';
 import { Room } from 'src/app/interfaces/room';
 import { UserData } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
 import { RoomService } from 'src/app/services/room.service';
+import { RoutingService } from 'src/app/services/routing.service';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.scss'],
+  animations: [fade, bounce, float],
 })
-export class RoomComponent implements OnInit {
+export class RoomComponent implements OnInit, OnDestroy {
+  readonly TOP_MIN = 100;
+  readonly TOP_MAX = 600;
+  readonly LEFT_MIN = 1;
+  readonly LEFT_MAX = 300;
+  top: number[];
+  left: number[];
   uid: number | any;
   roomId: string;
   room: Room;
-  elapsedDate;
-  limitDate;
-  progressNum;
-  progressPoo;
+  elapsedDate: number;
+  limitDate: number;
+  progressNum: number;
+  pooLength: Array<number>;
 
   user$: Observable<UserData> = this.authService.user$;
 
@@ -41,12 +50,18 @@ export class RoomComponent implements OnInit {
   constructor(
     private roomService: RoomService,
     private authService: AuthService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private routingService: RoutingService
   ) {}
 
   ngOnInit(): void {
     this.authService.user$.subscribe((user: any) => {
       this.uid = user.uid;
+    });
+    this.route.params.subscribe((params) => {
+      this.routingService.roomIdSubject.next(params.id);
+      console.log(params);
+      console.log(params.id);
     });
     this.roomId$.subscribe((id) => {
       this.roomId = id;
@@ -56,10 +71,38 @@ export class RoomComponent implements OnInit {
       this.elapsedDate = Math.floor(
         (this.now.getTime() - this.room.completedAt.toMillis()) / 86400000
       );
+
+      console.log(this.elapsedDate);
+
       this.limitDate = 21 - this.elapsedDate;
+
+      console.log(this.limitDate);
+
       this.progressNum = Math.min(300, (this.elapsedDate / 21) * 300);
-      this.progressPoo = this.room.pooCount * 100;
+
+      console.log(this.progressNum);
+
+      this.pooLength = new Array(this.room.pooCount)
+        .fill(null)
+        .map((_, i) => i + 1);
+
+      this.top = this.pooLength.map(() => {
+        return (
+          Math.floor(Math.random() * (this.TOP_MAX + 1 - this.TOP_MIN)) +
+          this.TOP_MIN
+        );
+      });
+      this.left = this.pooLength.map(() => {
+        return (
+          Math.floor(Math.random() * (this.LEFT_MAX + 1 - this.LEFT_MIN)) +
+          this.LEFT_MIN
+        );
+      });
     });
+  }
+
+  ngOnDestroy(): void {
+    this.routingService.roomIdSubject.unsubscribe();
   }
 
   finishedTask(): void {
