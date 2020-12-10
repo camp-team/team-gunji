236d14eca623e2bc2fc925887dfc785dd136bf4a
Yusuke Gunji <inochideutae@gmail.com>
2020-12-10 10:03:25 +0900

    feat(header): ルーム名表示追加

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Room } from '../interfaces/room';
 import { UserData } from '../interfaces/user';
 import { AuthService } from '../services/auth.service';
+import { RoomService } from '../services/room.service';
+import { RoutingService } from '../services/routing.service';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +13,29 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  uid: string;
   user$: Observable<UserData> = this.authService.user$;
+  roomId: string;
+  room$: Observable<Room> = this.user$.pipe(
+    switchMap((user) => {
+      this.uid = user.uid;
+      this.routingService.roomId$.subscribe((data) => {
+        this.roomId = data;
+        console.log(data);
+      });
+      return this.roomService.getRoom(this.uid, this.roomId);
+    })
+  );
 
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private roomService: RoomService,
+    private routingService: RoutingService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user$.subscribe((user) => {
+      this.uid = user.uid;
+    });
+  }
 }
